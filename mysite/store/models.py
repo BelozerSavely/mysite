72003d0e459bc8920e5a677cb61cf91e4b5feeb2
Anton ill <gidrontv1@gmail.com>
2024-03-22 21:04:31 +0400

    edited models

--- mysite/store/models.py
+++ mysite/store/models.py
@@ -35,6 +35,9 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
     image = models.ImageField(null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
     @property
     def imageURL(self):
         try:
